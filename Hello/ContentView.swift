02081105343434f91a65291a7e5f28952f26926c
Matt Schruben <schrubes@hotmail.com>
2024-10-08 08:46:51 -0700

    Initial Commit

---
+++ Hello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello
+//
+//  Created by Matt on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Matt on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
