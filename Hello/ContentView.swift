2199da40d081e7b46e1b36b1419ad5f05f675533
schrubes <schrubes@hotmail.com>
2025-05-27 12:29:51 -0700

    made the marble

--- Hello/ContentView.swift
+++ Hello/ContentView.swift
@@ -3,17 +3,18 @@
 //  Hello
 //
 //  Created by Matt on 10/8/24.
-//
-
+//s
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "speaker.zzz")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Paul this is you block of Marble")
+            
+            Image("marble")
         }
         .padding()
     }
